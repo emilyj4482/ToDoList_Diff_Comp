9a56b3fec792b37b813a2b3144b7166696e3400d
emilyj4482 <emilyj4482@gmail.com>
2023-06-28 02:45:21 +0900

    add a task mode 1차 구현

--- ToDoList_Diff_Comp/ToDoListViewController.swift
+++ ToDoList_Diff_Comp/ToDoListViewController.swift
@@ -16,9 +16,18 @@ class ToDoListViewController: UIViewController {
     
     @IBOutlet weak var textField: UITextField!
     
+    @IBOutlet weak var tfViewBottom: NSLayoutConstraint!
+    
     var vm = TaskViewModel.shared
     
     var index: Int?
+    
+    // Add a Task 버튼 tap 시 여러가지 기능 on / off
+    var addTaskMode: Bool = false {
+        didSet {
+            modeOnOff()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +35,7 @@ class ToDoListViewController: UIViewController {
         guard let index = index else { return }
         print(vm.lists[index])
         
-        barButtons()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(rightButtonTapped))
         
         // important list일 때는 task 추가 및 listname 수정 기능 잠금
         if index == 0 {
@@ -35,18 +44,34 @@ class ToDoListViewController: UIViewController {
         }
     }
     
-    private func barButtons() {
-        let rightBarButton = UIBarButtonItem(title: "Edit", style: .plain, target: self, action: #selector(rightButtonTapped))
-        rightBarButton.tintColor = .systemPink
-        navigationItem.rightBarButtonItem = rightBarButton
-    }
-    
     @objc func rightButtonTapped() {
         print("right btn tapped")
+        // test code
+        addTaskMode = false
     }
     
     @IBAction func addButtonTapped(_ sender: UIButton) {
         print("add btn tapped")
+        // test code
+        addTaskMode = true
     }
     
+}
+
+extension ToDoListViewController {
+    // Add a Task mode on / off 시 동작 구분
+    func modeOnOff() {
+        if addTaskMode == true {
+            navigationItem.rightBarButtonItem?.title = "Done"
+            addbutton.isHidden = true
+            tfView.isHidden = false
+            textField.becomeFirstResponder()
+            
+        } else {
+            navigationItem.rightBarButtonItem?.title = "Edit"
+            addbutton.isHidden = false
+            tfView.isHidden = true
+            textField.resignFirstResponder()
+        }
+    }
 }
