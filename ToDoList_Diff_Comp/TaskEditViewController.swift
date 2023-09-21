01fe65c5c74b59097bc85d0df53a33022391efdb
emilyj4482 <emilyj4482@gmail.com>
2023-09-22 04:18:21 +0900

    task 추가 textfield를 view로 분리하고 modal 처리

---
+++ ToDoList_Diff_Comp/TaskEditViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TaskEditViewController.swift
+//  ToDoList_Diff_Comp
+//
+//  Created by EMILY on 2023/09/22.
+//
+
+import UIKit
+
+class TaskEditViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
